ddc7168f88d2b98eebad77cc664e06c016ca9f6a
Franklin-pro <Franklin-pro>
2023-10-13 10:40:43 +0200

    asd

--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import newsController from '../controller/newsController'
+import verifyAccess from '../middlewares/verifyAccess'
 
 const router = express.Router()
 
-router.post("/",newsController.newsletter)
+router.post("/",verifyAccess("admin"),newsController.newsletter)
 router.get("/",newsController.getAllNews)
 router.delete("/",newsController.deleteAllNews)
 router.patch("/:id",newsController.updateNews)
